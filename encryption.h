b3e137b7a361c52ce10348e36c1bd057e8948852
thorntonj <thorntonj@uni.coventry.ac.uk>
2025-02-18 15:31:09 +0000

    First Commit

---
+++ encryption.h
@@ -0,0 +1,13 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+class Encryption {
+    public:
+    Encryption();
+    
+    ~Encryption();
+};
+
+#endif //ENCRYPTION_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/opencv.hpp>
+
+#include "encryption.h"
+using namespace cv;
+
+int main() {
+    Encryption encryption;
+
+    return 0;
+}
