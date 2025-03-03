61ab5367f2340b018366aada2cf16b54b1fa7f87
thorntonj <thorntonj@uni.coventry.ac.uk>
2025-03-03 17:33:48 +0000

    added some error handling

--- decode.cpp
+++ decode.cpp
@@ -68,7 +68,7 @@ fileName(path) {
   message_img = imread(filePath);
 
   if (message_img.empty()) {
-    cout << "Could not open or find " << filePath << ", make sure the image is a png." << endl;
+    cerr << "Could not open or find " << filePath << ", make sure the image is a png." << endl;
   }
   else {
     cout << "Opened file " << filePath << endl;
